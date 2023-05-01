e0f599900da3e22375f603043fcf83f626ed82d0
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-05-01 19:43:23 +0300

    local Storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -63,6 +63,21 @@ export class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     // const filteredContacts = this.state.contacts.filter(contact =>
     //   contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
